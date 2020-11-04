0898966aaf46da52ece22dde5d24b9fa24a1f8b9
Eshank Vaish <eshank.vaish@joshtechnologygroup.com>
2020-11-04 14:27:30 +0530

    [UA-2] Header - Overflow added

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,7 @@
 const hamburgerToggle = document.querySelector('.logo-toggle__hamburger');
 const crossToggle = document.querySelector('.logo-toggle__cross');
 const topNavList = document.querySelector('.top-nav-list');
+const body = document.querySelector('body');
 
 document.addEventListener('DOMContentLoaded', function() {
     function toggleMenu(clickedOn) {
@@ -8,10 +9,12 @@ document.addEventListener('DOMContentLoaded', function() {
             hamburgerToggle.classList.remove('toggle-active');
             crossToggle.classList.add('toggle-active');
             topNavList.classList.add('toggle-active');
+            body.classList.add('overflow-hidden');
         } else {
             hamburgerToggle.classList.add('toggle-active');
             crossToggle.classList.remove('toggle-active');
             topNavList.classList.remove('toggle-active');
+            body.classList.remove('overflow-hidden');
         }
     }
     hamburgerToggle.addEventListener('click', () => {
